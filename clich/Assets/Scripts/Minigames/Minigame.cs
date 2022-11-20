433a50351adba5cd5a154e3113374f0e59dc6306
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2022-11-19 20:12:15 -0800

    Add a comment

--- clich/Assets/Scripts/Minigames/Minigame.cs
+++ clich/Assets/Scripts/Minigames/Minigame.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public abstract class Minigame : MonoBehaviour
 {
     [HideInInspector] public bool started = false;
-    public abstract void StartMinigame();
+    public abstract void StartMinigame(); //this is here so if we want to have the game start a bit after the transition we can do that, please use this instead of Start()
 
     protected void EndMinigame()
     {
